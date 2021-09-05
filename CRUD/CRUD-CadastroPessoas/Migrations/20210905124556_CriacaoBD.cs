f4fe4daa705befc00c34ad3b51e17cca9cf1f008
Marcus Rolim <marcusrolimcc@gmail.com>
2021-09-05 16:36:31 -0400

    Adicionado CRUD-CadastroPessoas em C#

---
+++ CRUD/CRUD-CadastroPessoas/Migrations/20210905124556_CriacaoBD.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CadastroPessoas.Migrations
+{
+    public partial class CriacaoBD : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pessoas",
+                columns: table => new
+                {
+                    PessoaId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SobreNome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CPF = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Idade = table.Column<int>(type: "int", nullable: false),
+                    Telefone = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pessoas", x => x.PessoaId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pessoas");
+        }
+    }
+}
---
+++ CRUD/CRUD-CadastroPessoas/Models/Pessoa.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroPessoas.Models
+{
+    public class Pessoa
+    {
+        public int PessoaId { get; set; }
+
+        public string Nome { get; set; }
+
+        public string SobreNome { get; set; }
+
+        public string CPF { get; set; }
+
+        public int Idade { get; set; }
+
+        public string Telefone { get; set; }
+
+
+
+
+    }
+}
