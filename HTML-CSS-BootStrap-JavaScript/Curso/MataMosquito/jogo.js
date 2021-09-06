1b79ce3fc1f64a621fddb5fe12b180384dcbb6c1
Marcus Rolim <marcusrolimcc@gmail.com>
2021-09-06 17:19:39 -0400

    Estado e Cidade

--- HTML-CSS-BootStrap-JavaScript/Curso/MataMosquito/jogo.js
+++ HTML-CSS-BootStrap-JavaScript/Curso/MataMosquito/jogo.js
@@ -15,3 +15,8 @@ var posicaoX  = Math.floor(Math.random() * altura);
 var posicaoY = Math.floor(Math.random() * largura);
 
 console.log(posicaoX, posicaoY);
+
+var mosquito = document.createElement('img');
+mosquito.src = 'imagens/mosquito.png';
+
+document.body.appendChild(mosquito);
--- Java/Cursos/SistemaDeCompraFullStack/src/main/java/com/br/marcusrolim/cursojavafullstack/CursoJavaFullStackApplication.java
+++ Java/Cursos/SistemaDeCompraFullStack/src/main/java/com/br/marcusrolim/cursojavafullstack/CursoJavaFullStackApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.br.marcusrolim.cursojavafullstack.domain.Categoria;
+import com.br.marcusrolim.cursojavafullstack.domain.Cidade;
+import com.br.marcusrolim.cursojavafullstack.domain.Estado;
 import com.br.marcusrolim.cursojavafullstack.domain.Produto;
 import com.br.marcusrolim.cursojavafullstack.repositories.CategoriaRepository;
+import com.br.marcusrolim.cursojavafullstack.repositories.CidadeRepository;
+import com.br.marcusrolim.cursojavafullstack.repositories.EstadoRepository;
 import com.br.marcusrolim.cursojavafullstack.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class CursoJavaFullStackApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoJavaFullStackApplication.class, args);
@@ -41,9 +51,23 @@ public class CursoJavaFullStackApplication implements CommandLineRunner {
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
-		
-		
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		
 		}
 }
