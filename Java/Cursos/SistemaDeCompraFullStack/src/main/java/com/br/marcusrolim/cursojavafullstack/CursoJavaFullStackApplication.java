326d188efb034ba75ebd2b3049a678662f02965b
Marcus Rolim <marcusrolimcc@gmail.com>
2021-09-07 11:28:54 -0400

    Cliente, TipoCliente, Telefones, e Endereços

--- Java/Cursos/SistemaDeCompraFullStack/src/main/java/com/br/marcusrolim/cursojavafullstack/CursoJavaFullStackApplication.java
+++ Java/Cursos/SistemaDeCompraFullStack/src/main/java/com/br/marcusrolim/cursojavafullstack/CursoJavaFullStackApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.br.marcusrolim.cursojavafullstack.domain.Categoria;
 import com.br.marcusrolim.cursojavafullstack.domain.Cidade;
+import com.br.marcusrolim.cursojavafullstack.domain.Cliente;
+import com.br.marcusrolim.cursojavafullstack.domain.Endereco;
 import com.br.marcusrolim.cursojavafullstack.domain.Estado;
 import com.br.marcusrolim.cursojavafullstack.domain.Produto;
+import com.br.marcusrolim.cursojavafullstack.domain.enums.TipoCliente;
 import com.br.marcusrolim.cursojavafullstack.repositories.CategoriaRepository;
 import com.br.marcusrolim.cursojavafullstack.repositories.CidadeRepository;
+import com.br.marcusrolim.cursojavafullstack.repositories.ClienteRepository;
+import com.br.marcusrolim.cursojavafullstack.repositories.EnderecoRepository;
 import com.br.marcusrolim.cursojavafullstack.repositories.EstadoRepository;
 import com.br.marcusrolim.cursojavafullstack.repositories.ProdutoRepository;
 
@@ -30,6 +35,12 @@ public class CursoJavaFullStackApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoJavaFullStackApplication.class, args);
@@ -68,6 +79,16 @@ public class CursoJavaFullStackApplication implements CommandLineRunner {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apt 203", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 		
 		}
 }
